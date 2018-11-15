ac6bd09c5b4c108c274c00125ab28f70581bc608
lcyin <laichunyinkevin@gmail.com>
2018-11-15 13:13:27 +0800

    Commit message

---
+++ 033-knex-JS/myApp/migrations/20181115124206_new-change-on-database.js
@@ -0,0 +1,16 @@
+
+exports.up = function(knex, Promise) {
+    return knex.schema.createTable('users', (table)=>{
+        table.increments();
+        table.string('name');
+        table.string('email');
+        table.string('password');
+        table.boolean('active');
+        table.timestamps(false, true);
+  
+    });
+  };
+  
+  exports.down = function(knex, Promise) {
+      return knex.schema.dropTable('users')
+  };
